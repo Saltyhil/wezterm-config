332999e2bdc56b882692e6edfa6f15211f0cfac1
Saltyhil <sahilmohd613@gmail.com>
2024-08-23 20:38:08 +0530

    wezterm terminal config

---
+++ wezterm.lua
@@ -0,0 +1,37 @@
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+
+local config = wezterm.config_builder()
+-- Config_Stars_From_Here 
+
+-- Configuring act command 
+local act = wezterm.action
+
+-- Choosing colourscheme
+config.color_scheme = 'Gruvbox dark, medium (base16)'
+
+-- Font changing 
+config.font = wezterm.font {
+ family = 'JetBrains Mono',
+ harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
+ }
+
+-- Font size and tab function
+config.font_size = 13
+config.enable_tab_bar = false
+
+-- customizing cursor 
+config.default_cursor_style = 'BlinkingBar'
+config.animation_fps = 1
+
+--Command palatee (CTRL+SHIFT+P)
+config.keys = {
+ {
+ key = 'P',
+ mods = 'CTRL',
+ action = wezterm.action.ActivateCommandPalette,
+ },
+}
+
+return config
