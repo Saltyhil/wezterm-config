c4c99a477ad9d8e44a2b83fe256976e604982e05
Saltyhil <sahilmohd613@gmail.com>
2024-10-01 23:26:56 +0530

    Another change

--- wezterm.lua
+++ wezterm.lua
@@ -25,12 +25,6 @@ config.default_cursor_style = 'BlinkingBar'
 config.animation_fps = 1
 
 --Command palatee (CTRL+SHIFT+P)
-config.keys = {
- {
- key = 'P',
- mods = 'CTRL',
- action = wezterm.action.ActivateCommandPalette,
- },
 }
 
 return config
